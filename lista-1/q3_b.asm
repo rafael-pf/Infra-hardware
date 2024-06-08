a8e3aabeb6aa5dd83a88fc6b98c500e76233e4f0
Rafael <rafael.paz@citi.org.br>
2024-06-08 20:32:06 -0300

    letra a da 3 feita

---
+++ lista-1/q3_b.asm
@@ -0,0 +1,30 @@
+lw x9, MaxIterations
+lw x18, N1
+lw x19, N2
+addi x20, x0, 0
+addi x5, x0, 0
+
+mult:
+    
+    addi x5, x5, 1 # Incrementa contador
+	
+
+    # Test multiplier (bit mask)
+    andi x6, x18, 1
+    beq x6, x0, shift
+    # Add multiplicand to product and place the result in Product register
+    add x20, x20, x19
+    
+    shift:
+        
+		slli x19, x19, 1 # Shift the multiplicand register left 1 bit
+		
+        srli x18, x18, 1 # Shift the multiplicand register right 1 bit
+    
+    bne x9, x5, mult
+
+halt
+
+N1: .word 15
+N2: .word 20
+MaxIterations: .word 32
